2d5521293a7e905efcec6acf4a1e81b8e4997265
naggarx <72861717+naggarx@users.noreply.github.com>
2023-05-13 23:12:11 +0200

    Update Connect-4.py

--- Connect-4.py
+++ Connect-4.py
@@ -1,8 +1,21 @@
 import numpy as np
+import pygame 
+import sys
+BLUE = (0,0,255)
 def create_board():
     board = np.zeros((6,7))
     return board
 board = create_board()
+pygame.init()
+SQUARESIZE = 100
+width = 7 * SQUARESIZE
+height = 7 * SQUARESIZE
+
+size = (width , height)
+radius = int(SQUARESIZE / 2 - 5)
+screen = pygame.display.set_mode(size)
+
+
 #NEGO
 def drop(board , row , col , player):
     board[row][col] = player
@@ -12,8 +25,6 @@ def get_next(board,col):
     for r in range(6) :
         if board[r][col] == 0:
             return r
-def print_board(board):
-    print(np.flip(board , 0));
 def winning_move(board , player):
     #Check Horiz.
     for c in range(7-3):
@@ -35,10 +46,15 @@ def winning_move(board , player):
         for r in range (3,6):
             if board[r][c] == player and board[r+1][c+1] == player and board[r+2][c+2] == player and board[r+3][c+3] == player:
                 return True
-    
-    
+def draw_board(board):
+    for c in range(7):
+        for r in range(6):
+              pygame.draw.rect(screen, BLUE , (c*SQUARESIZE ,  r*SQUARESIZE + SQUARESIZE, SQUARESIZE , SQUARESIZE) )
+              pygame.draw.circle(screen, (0,0,0) ,  (c*SQUARESIZE + int(SQUARESIZE/2),  r*SQUARESIZE + SQUARESIZE + int(SQUARESIZE/2) ) , radius)
 EndOfGame = False
 turn = 0
+draw_board(board)
+pygame.display.update()
 while not EndOfGame:
     #First Player
     if (turn == 0):
